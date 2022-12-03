1a4fe79b76dcddb0550486e00fe594756d02d04a
Filipe Almeida <118920970+Balanight@users.noreply.github.com>
2022-12-03 03:50:51 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "HamburgerB36px.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "CloseP25px.svg";
+    }
+}
