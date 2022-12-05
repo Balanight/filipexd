a3c82fa370589dc1c5e72bac60277bfa6c95d08e
Filipe Almeida <118920970+Filipei@users.noreply.github.com>
2022-12-05 15:50:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.menu');
+    let menuMobile = document.querySelector('.sidebar, .menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "HamburgerB36px.svg";
